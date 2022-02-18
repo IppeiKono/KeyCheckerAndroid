5dd3aa3a9162431716179f8bc6b8df2fd3fd9953
IppeiKono <konoippei@keio.jp>
2022-02-18 23:58:44 +0900

    ドラムロールを増やして2つでコード出力できるようにした

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -16,21 +16,28 @@ class MainActivity : AppCompatActivity() {
         var helloText : TextView = findViewById(R.id.textView1)
         var helloButton : Button = findViewById(R.id.button1)
         //ドラムロール表示用の配列作成
-        val fruits = arrayOf("C", "D", "E")
+        val code1Left = arrayOf("C", "D", "E", "F", "G", "A", "B")
+        val code1Right = arrayOf("", "m",  "♭", "#", "#m", "dim", "#dim")
+
         //NumberPickerを取得
-        val picker = findViewById<NumberPicker>(R.id.picker)
+        val picker1Left = findViewById<NumberPicker>(R.id.picker1Left)
+        val picker1Right = findViewById<NumberPicker>(R.id.picker1Right)
         //配列のインデックス最小、最大を指定
-        picker.minValue = 0
-        picker.maxValue = fruits.size - 1
+        picker1Left.minValue = 0
+        picker1Left.maxValue = code1Left.size - 1
+        picker1Right.minValue = 0
+        picker1Right.maxValue = code1Right.size - 1
         //NumberPickerに配列をセットする
-        picker.displayedValues = fruits
+        picker1Left.displayedValues = code1Left
+        picker1Right.displayedValues = code1Right
 
         helloButton.setOnClickListener{
             //選択されている項目のインデックス番号を取得
-            val index = picker.value
+            val index1L = picker1Left.value
+            val index1R = picker1Right.value
             //フルーツの配列からインデックス番号を指定して取得
-            val fruit = fruits[index]
-            helloText.text = fruit
+            val code = code1Left[index1L] + code1Right[index1R]
+            helloText.text = code
         }
     }
 }
